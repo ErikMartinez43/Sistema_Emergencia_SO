15b4af7cc209b5df9c4b905bba384389b7b1ba41
Erik Martinez <erikmartinez4321@outlook.com>
2025-04-04 12:01:04 -0600

    definiendo constantes y funciones necesarias de memoria compartida

---
+++ include/memoria_compartida.h
@@ -0,0 +1,14 @@
+#ifndef MEMORIA_COMPARTIDA_H
+#define MEMORIA_COMPARTIDA_H
+
+#include <sys/types.h>
+
+int crear_clave_memoria(key_t clave, size_t tamaño);
+
+void* get_memoria(int id);
+
+void liberar_memoria(void* ptr);
+
+void eliminar_memoria(int id);
+
+#endif
