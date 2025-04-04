7adea1a7e873de73a2e1026dc204cf7191a195b9
Erik Martinez <erikmartinez4321@outlook.com>
2025-04-04 13:43:58 -0600

    inicializando semaforos y metodos

---
+++ include/semaforos.h
@@ -0,0 +1,16 @@
+#ifndef SEMAFOROS_H
+#define SEMAFORS_H
+
+#include <sys/types.h>
+
+int crear_semaforos(key_t clave, int cantidad);
+
+void inicializar_semaforo(int id, int num_semaforo, int valor);
+
+void tomar_semaforo(int id, int num_semaforo);
+
+void liberar_semaforo(int id, int num_semaforo);
+
+void eliminar_semaforo(int id);
+
+#endif;
