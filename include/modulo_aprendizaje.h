4c33c6860e8722f35ef981c4d23b96b24a9f3b0b
Erik Martinez <erikmartinez4321@outlook.com>
2025-04-06 15:48:05 -0600

    definicion base modulo de aprendizaje

---
+++ include/modulo_aprendizaje.h
@@ -0,0 +1,14 @@
+#ifndef APRENDIZAJE_H
+#define APRENDIZAJE_H
+
+#include "tipos.h"
+
+void aprendizaje_recomendar_respuesta(
+    MemoriaCompartida* mem,
+    int codigo_zona;
+    int tipo_llamada;
+    int* tipo_unidad;
+    int* tipo_vehiculo;
+ );
+
+#endif //fin de APRENDIZAJE_H
