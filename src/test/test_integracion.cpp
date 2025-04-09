d9bcb51df142ca5f7d34c6bc0e6e5de6d1b2599e
Erik Martinez <erikmartinez4321@outlook.com>
2025-04-09 13:36:20 -0600

    defaul

--- src/test/test_integracion.cpp
+++ src/test/test_integracion.cpp
@@ -1,49 +1,29 @@
-#include "memoria_compartida.h"
-#include "modulo_aprendizaje.h"
-#include "interfaces.h"
-#include <iostream>
-#include <string.h>
+#include "semaforos.h"
 
-using namespace std;
-
-int main() {
-    int shm_id = crear_clave_memoria(1234, sizeof(MemoriaCompartida));
-    if (shm_id == -1) {
-        perror("Error creando memoria");
+int main(){
+    //Crear conjunto de semaforos
+    key_t clave = ftok("/tmp", 'S');
+    GestionSemaforos gs = crear_conjunto_semaforos(clave, 3);
+    
+    if(gs.sem_id == -1)
+    {
         return 1;
     }
 
-    MemoriaCompartida* mem = (MemoriaCompartida*) obtener_memoria(shm_id);
-    if (mem == nullptr) {
-        perror("Error obteniendo memoria");
-        return 1;
+    //incializar semaforo
+    if(gs.creado)
+    {
+        inicializar_semaforo(&gs, 0, 1); //semaforo binario
+        inicializar_semaforo(&gs, 1, 5); //semaforo contador
     }
 
-    // === Inicializar zonas ===
-    ZonaRiesgo zona1 = {78, "Colonia El Pedregal", ZONA_SUR, ACTIVIDAD_CRIMEN_ORGANIZADO, RIESGO_ALTO, TERRENO_MONTANIOSO};
-    ZonaRiesgo zona2 = {22, "Barrio El Centro", ZONA_CENTRO, ACTIVIDAD_NONE, RIESGO_BAJO, TERRENO_URBANO};
+    //uso tipico
+    if(tomar_semaforo(&gs, 0, 1000)) //timeout de 1 s
+    {   //seccion critica
+        liberar_semaforo(&gs, 0);
+    }
 
-    mem->zonas[0] = zona1;
-    mem->zonas[1] = zona2;
-
-    // === Simular llamadas ===
-    int tipo_unidad, tipo_vehiculo;
-
-    cout << "\n===== LLAMADA MÉDICA desde zona 78 =====" << endl;
-    aprendizaje_recomendar_respuesta(mem, 78, LLAMADA_MEDICA, &tipo_unidad, &tipo_vehiculo);
-    cout << "Unidad: " << obtener_tipo_unidad(tipo_unidad) << endl;
-    cout << "Vehículo: " << tipo_vehiculo << endl;
-
-    cout << "\n===== LLAMADA POLICIAL desde zona 22 =====" << endl;
-    aprendizaje_recomendar_respuesta(mem, 22, LLAMADA_POLICIAL, &tipo_unidad, &tipo_vehiculo);
-    cout << "Unidad: " << obtener_tipo_unidad(tipo_unidad) << endl;
-    cout << "Vehículo: " << tipo_vehiculo << endl;
-
-    cout << "\n===== LLAMADA COMBINADA TOTAL desde zona 78 =====" << endl;
-    aprendizaje_recomendar_respuesta(mem, 78, LLAMADA_TOTAL, &tipo_unidad, &tipo_vehiculo);
-    cout << "Unidad: " << obtener_tipo_unidad(tipo_unidad) << endl;
-    cout << "Vehículo: " << tipo_vehiculo << endl;
-
-    eliminar_memoria(shm_id);
+    //limpieza
+    eliminar_conjunto_semaforos(&gs);
     return 0;
 }
